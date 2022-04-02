0e70dd816213c738184584a5f3ee08a6b40d15b5
Oskar Pawica <pawicao@gmail.com>
2022-04-02 16:24:08 +0200

    Material UI

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './styles.css';
+import {Button} from "@mui/material";
 
 function Home() {
   return (
     <div>
       <h1>Your travel app</h1>
       <h2>Make Remote Greater</h2>
+      <Button>LOGIN</Button>
     </div>
   );
 }
